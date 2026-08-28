9be7b7f21057aea36fa63ef8adca26d903f3efb4
chetx27 <chethana.workspace@gmail.com>
2026-08-28 19:37:22 +0530

    Export the new baselines from the baselines package.

--- baselines/__init__.py
+++ baselines/__init__.py
@@ -1,11 +1,19 @@
 """Baselines live in echo.acquisition; this package is a stable import path."""
 
+from echo.acquisition.diversity import diversity_score
 from echo.acquisition.expected_improvement import expected_improvement
+from echo.acquisition.greedy import greedy_mean
 from echo.acquisition.information_gain import local_information_gain
+from echo.acquisition.thompson import thompson_meanfield
+from echo.acquisition.ucb import gp_ucb
 from echo.acquisition.uncertainty import predictive_uncertainty
 
 __all__ = [
+    "diversity_score",
     "expected_improvement",
+    "greedy_mean",
+    "gp_ucb",
     "local_information_gain",
     "predictive_uncertainty",
+    "thompson_meanfield",
 ]
